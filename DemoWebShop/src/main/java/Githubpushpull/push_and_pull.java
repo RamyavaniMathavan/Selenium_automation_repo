ba17ce8a9ff0b6b9ce8b2203378f5d56431eefd2
Ramyavani <Ramyavani@RAMMI>
2025-07-01 13:17:17 +0530

    push_and_pull)_demo

---
+++ DemoWebShop/src/main/java/Githubpushpull/push_and_pull.java
@@ -0,0 +1,16 @@
+package Githubpushpull;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class push_and_pull {
+	
+	@Test
+	public void demo() {
+	
+	WebDriver driver= new ChromeDriver();
+
+
+}
+}
